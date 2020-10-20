8a7d5d8f29eb68183fb753cadd0a6ae778a3a101
Le Vinh Trong <trong.levinh@gotadi.com>
2020-10-20 13:59:21 +0700

    init

---
+++ GotadiVIB/GotadiVIB.h
@@ -0,0 +1,18 @@
+//
+//  GotadiVIB.h
+//  GotadiVIB
+//
+//  Created by Gotadi-Mac on 10/20/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GotadiVIB.
+FOUNDATION_EXPORT double GotadiVIBVersionNumber;
+
+//! Project version string for GotadiVIB.
+FOUNDATION_EXPORT const unsigned char GotadiVIBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GotadiVIB/PublicHeader.h>
+
+
---
+++ GotadiVIB/TEST.swift
@@ -0,0 +1,22 @@
+//
+//  TEST.swift
+//  GotadiVIB
+//
+//  Created by Gotadi-Mac on 10/20/20.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
